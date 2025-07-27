9b5f74f12ae5f66908b9d814f7fafd618f67267c
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2025-07-27 22:27:03 +0200

    Add mapping and repository classes for User and Household entities

---
+++ auth-service/src/main/kotlin/com/armadoon/homebuddy/auth/repository/UserRepository.kt
@@ -0,0 +1,22 @@
+package com.armadoon.homebuddy.auth.repository
+
+import com.armadoon.homebuddy.auth.entity.User
+import io.micronaut.data.annotation.Query
+import io.micronaut.data.annotation.Repository
+import io.micronaut.data.repository.CrudRepository
+import java.util.*
+
+@Repository
+interface UserRepository : CrudRepository<User, Long> {
+
+    fun findByUsername(username: String): Optional<User>
+
+    fun findByEmail(email: String): Optional<User>
+
+    fun findByHouseholdIdAndIsActive(householdId: Long, isActive: Boolean = true): List<User>
+
+    fun existsByUsernameOrEmail(username: String, email: String): Boolean
+
+    @Query("UPDATE User u SET u.householdId = :householdId WHERE u.id = :userId")
+    fun updateHouseholdId(userId: Long, householdId: Long): Int
+}
